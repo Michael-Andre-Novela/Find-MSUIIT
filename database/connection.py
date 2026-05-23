bf5805ac3f9c1896eb095162d38aaeb55764ac88
Michael Andre Novela <michaelandre.novela@g.msuiit.edu.ph>
2026-05-23 16:43:09 +0800

    feat(database): initialize schema step 1 with independent tables
    - Created `database/connection.py` to handle SQLite connections.
    - Added `os.makedirs` guard to guarantee the database directory exists dynamically.
    - Built independent parent tables: `categories` and `constituents`.
    - Normalized `category_id` to its singular form to match the ER map layout.
    - Enforced strict relational rules via `PRAGMA foreign_keys = ON;`.

--- database/connection.py
+++ database/connection.py
@@ -1,1 +1,33 @@
+import sqlite3
+import os
 
+
+def independent_tables():
+        #To ensure that the db folder exists before connecting
+        os.makedirs("database", exist_ok=True)
+
+        conn = sqlite3.connect("database/find_iit.db")
+        cursor = conn.cursor()
+
+        cursor.execute("PRAGMA foreign_keys = ON;")
+
+        #Creating the Categories tables
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS categories(
+                       category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       category_name TEXT NOT NULL UNIQUE
+                       );
+                    """)
+        #Creating the Constituents tables
+
+        cursor.execute(""" CREATE TABLE IF NOT EXISTS constituents(
+                    constituent_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    id_number TEXT NOT NULL UNIQUE,
+                    name TEXT NOT NULL,
+                    contact_email TEXT NOT NULL,
+                    contact_phone TEXT
+                     );""")
+        conn.commit()
+        conn.close()
+
+        
